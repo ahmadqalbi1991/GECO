0876d48a088f1beff76601d752827ddb35ba0d4a
ahmadqalbi1991 <ahmadqalbi2@gmail.com>
2021-09-12 01:05:03 +0500

    Dashboard Update

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Order;
 
 class AdminController extends Controller
 {
@@ -19,6 +20,36 @@ class AdminController extends Controller
         }
 
         $data['title'] = 'Dashboard';
+        $orders = Order::all();
+        $categories_wise_sale = [];
+        $categories = [
+            'console' => 0, 'headphones' => 0, 'gamepad' => 0, 'gamecontroller' => 0, 'games' => 0
+        ];
+
+        foreach ($orders as $key => $order) {
+            $items = $order->items;
+            foreach ($items as $key1 => $item) {
+                $product = $item->product;
+                foreach ($categories as $key2 => $category) {
+                    if ($product->category == $key2) {
+                        $sum = $category + 1;
+                        $categories_wise_sale[$key2] = $sum;
+                        $categories[$key2] = $sum;
+                    } else {
+                        $categories_wise_sale[$key2] = $category;
+                    }
+                }
+            }
+        }
+
+        $sales = [];
+        $i = 0;
+        foreach($categories_wise_sale as $category) {
+            array_push($sales, $category);
+        }
+
+        $data['sales'] = '[' . implode(',', $sales) . ']';
+
         return view('admin.pages.dashboard')->with($data);
     }
 
--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -718,4 +718,147 @@
             </div>
         </div>
     </div>
+    @push('scripts')
+        <script>
+            var ctx = document.getElementById("chart-bars").getContext("2d");
+
+            new Chart(ctx, {
+                type: "bar",
+                data: {
+                    labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                    datasets: [{
+                        label: "Sales",
+                        tension: 0.4,
+                        borderWidth: 0,
+                        pointRadius: 0,
+                        backgroundColor: "#fff",
+                        data: [450, 200, 100, 220, 500, 100, 400, 230, 500],
+                        maxBarThickness: 6
+                    }, ],
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    legend: {
+                        display: false,
+                    },
+                    tooltips: {
+                        enabled: true,
+                        mode: "index",
+                        intersect: false,
+                    },
+                    scales: {
+                        yAxes: [{
+                            gridLines: {
+                                display: false,
+                            },
+                            ticks: {
+                                suggestedMin: 0,
+                                suggestedMax: 500,
+                                beginAtZero: true,
+                                padding: 0,
+                                fontSize: 14,
+                                lineHeight: 3,
+                                fontColor: "#fff",
+                                fontStyle: 'normal',
+                                fontFamily: "Open Sans",
+                            },
+                        }, ],
+                        xAxes: [{
+                            gridLines: {
+                                display: false,
+                            },
+                            ticks: {
+                                display: false,
+                                padding: 20,
+                            },
+                        }, ],
+                    },
+                },
+            });
+
+            var ctx2 = document.getElementById("chart-line").getContext("2d");
+
+            var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+            gradientStroke1.addColorStop(1, 'rgba(203,12,159,0.2)');
+            gradientStroke1.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+            gradientStroke1.addColorStop(0, 'rgba(203,12,159,0)'); //purple colors
+
+            var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+            gradientStroke2.addColorStop(1, 'rgba(20,23,39,0.2)');
+            gradientStroke2.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+            gradientStroke2.addColorStop(0, 'rgba(20,23,39,0)'); //purple colors
+
+
+            new Chart(ctx2, {
+                type: "line",
+                data: {
+                    labels: ["console", "headphones", "gamepad", "gamrcontroller", "games"],
+                    datasets: [{
+                        tension: 0.4,
+                        borderWidth: 0,
+                        pointRadius: 0,
+                        borderColor: "#cb0c9f",
+                        borderWidth: 3,
+                        backgroundColor: gradientStroke1,
+                        data: {{ $sales }},
+                        maxBarThickness: 6
+
+                    }],
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    legend: {
+                        display: false,
+                    },
+                    tooltips: {
+                        enabled: true,
+                        mode: "index",
+                        intersect: false,
+                    },
+                    scales: {
+                        yAxes: [{
+                            gridLines: {
+                                borderDash: [2],
+                                borderDashOffset: [2],
+                                color: '#dee2e6',
+                                zeroLineColor: '#dee2e6',
+                                zeroLineWidth: 1,
+                                zeroLineBorderDash: [2],
+                                drawBorder: false,
+                            },
+                            ticks: {
+                                suggestedMin: 0,
+                                suggestedMax: 500,
+                                beginAtZero: true,
+                                padding: 10,
+                                fontSize: 11,
+                                fontColor: '#adb5bd',
+                                lineHeight: 3,
+                                fontStyle: 'normal',
+                                fontFamily: "Open Sans",
+                            },
+                        }, ],
+                        xAxes: [{
+                            gridLines: {
+                                zeroLineColor: 'rgba(0,0,0,0)',
+                                display: false,
+                            },
+                            ticks: {
+                                padding: 10,
+                                fontSize: 11,
+                                fontColor: '#adb5bd',
+                                lineHeight: 3,
+                                fontStyle: 'normal',
+                                fontFamily: "Open Sans",
+                            },
+                        }, ],
+                    },
+                },
+            });
+        </script>
+    @endpush
 @endsection
